953abf8cf56ae02e0218db667febe77d42156e10
integerJI <jjs9536@gmail.com>
2020-04-15 20:58:52 +0900

    검색 페이지 수정, 글쓰기 form 형식으로 변경

---
+++ myApp/form.py
@@ -0,0 +1,21 @@
+# myApp/form.py
+
+from django import forms
+from django.db import models
+
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['main_text', 'create_img']
+
+        widgets = {
+            'main_text': forms.TextInput(attrs={'class': 'form-control'}),
+            'create_img' : forms.ClearableFileInput(attrs={'class': 'form-control-file'}),
+        }
+
+        labels = {
+            'main_text': '본문 내용',
+            'create_img': '사진',
+        }
--- myApp/views.py
+++ myApp/views.py
@@ -36,14 +36,17 @@ def index(request):
     return render(request, 'index.html', context=context)
 
 def post(request):
-    if request.method == 'POST':
-        post = Post()
-        post.main_text = request.POST['main_text']
-        post.create_img = request.FILES['create_img']
-        post.create_user = User.objects.get(username = request.user.get_username())
-        post.save()
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        
+        if form.is_valid(): 
+            post = form.save(commit = False)
+            post.create_user = User.objects.get(username = request.user.get_username())
+            post.save()
         return redirect(reverse('index'))
-    return render(request, 'post.html')
+    else:
+        form = PostForm() 
+        return render(request, 'post.html', {'form' : form})  
 
 def detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
